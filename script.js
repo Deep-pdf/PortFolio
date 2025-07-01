fe52d50daf1f35ab829459c93b0a1f8cae6485a4
Soumodeep <soumodeepsantra5@gmail.com>
2025-07-01 13:45:16 +0530

    nothing much

---
+++ script.js
@@ -0,0 +1,11 @@
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logoS');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const splash = document.getElementById('splash');
+  setTimeout(() => {
+    splash.classList.add('slide-up');
+  }, 1000);
+
+});
+
